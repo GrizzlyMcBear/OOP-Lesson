e1a15fc0800c285d4f231e3401b090cd1c18ad9e
Ronen Cohen <sronenco@gmail.com>
2021-05-10 10:17:42 +0300

    Added a project with a unique namespace for the drill on the 2021.05.11
    Added the students' tasks to `Program.cs` in project `Main`.

--- OOP_Lesson/Main/Program.cs
+++ OOP_Lesson/Main/Program.cs
@@ -6,9 +6,43 @@ namespace Main
 	{
 		static void Main(string[] args)
 		{
-			string carPlateNumber = "123456";
+			/*	Create the following classes, which should inherit from class `Vehicle`:
+			 *	1. Car
+			 *	2. Airplane
+			 *	3. Submarine
+			 */
 
-			Vehicle myCar = new Car("VehicleCarId");
+			/*	Add fields into class `Vehicle` accessible only from within itself
+			 *	1. An identifying text
+			 *	2. An integer representing how cool the vehicle is
+			 *	3. Is a superhero uses this vehicle
+			 */
+
+			/*	Add fields to class `Vehicle` accessible to all of its inheriting classes:
+			 *	1. Representation of the license plate number
+			 *	2. The amount of wheels it has
+			 *	3. The vehicle's maximum speed (might not be an integer)
+			 */
+
+			/*	+ Write a public method `DoStunt` in class `Vehicle`
+			 *	+ Implement it differently for each inheriting class:
+			 *	1. Car
+			 *	2. Airplane
+			 *	3. Submarine
+			 */
+
+			/*	Add a field to class `Vehicle` which would represent its make year, then:
+			 *	+ Everyone would be able to read it
+			 *	+ Everyone would be able to update it but only with values between 1901 and the current year
+			 */
+
+			/*	Do the same and convert the following fields into properties:
+			 *	1. In class `Car`: `plateNumber`
+			 *	2. In class `Airplane`: `wheelsAmount`
+			 *	3. In class `Submarine`: `maxSpeed`
+			 */
+
+			
 		}
 	}
 }
---
+++ OOP_Lesson/StudentsSolutions/2021.05.11/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentsSolutions._2021._05._11
+{
+	public class Vehicle
+	{
+		//public virtual void DoStunt()
+		//{
+
+		//}
+	}
+}
